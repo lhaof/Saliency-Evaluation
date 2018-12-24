0174d95b59f10f8bc0c8549d25de121f1c4eeea3
lhaof <lhaof@qq.com>
2018-12-24 16:17:26 +0800

    Update salmetric.py

--- salmetric.py
+++ salmetric.py
@@ -3,9 +3,10 @@ import salmetric_cpp
 
 if __name__ == '__main__':
 	print('done.')
-	predpath = '../deepMDC/deeplabv3plus_MDC/tmp_pred_mobilenetv2_epoch-8_on_HKU-IS/'
-	gtpath = '../deepMDC/deeplabv3plus_MDC/tmp_gt_mobilenetv2_epoch-8_on_HKU-IS/'
+	predpath = '../YOUR_SALIENCY_MAPS_PATH/'
+	gtpath = '../YOUR_GROUND_TRUTH_PATH/'
 	metrics = torch.zeros(4)
 	fmeasures = torch.zeros(256)
 	salmetric_cpp.evaluate(predpath, gtpath, 256, 10, metrics, fmeasures, 0)
+	# metrics contains maxf, precision, recall and mae.
 	print(metrics)
